67d00b98440bf9f5c8bdd7c85d613f304d31558d
Vsanotskyi <vsanotskyi@gmail.com>
2024-03-27 00:00:35 +0200

    c9

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -5,6 +5,8 @@ import {Box, Typography, Button} from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 
 import css from './Error.module.css';
+import {useAppDispatch, useSearchToggle} from '../../hooks';
+import {toggleShowSearch} from '../../store/search';
 
 interface IProps {
     message: string;
@@ -12,8 +14,12 @@ interface IProps {
 
 const Error: FC<IProps> = ({message}) => {
     const navigate = useNavigate();
+    const dispatch = useAppDispatch();
+
+    const searchToggle = useSearchToggle().searchToggle;
 
     const handleClick = () => {
+        searchToggle && dispatch(toggleShowSearch());
         navigate('/');
     };
 
